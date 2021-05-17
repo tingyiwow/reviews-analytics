27947683c7c88f56443b7d041fe9754392076796
tingyiwow <r07444005@ntu.edu.tw>
2021-05-17 16:52:15 +0800

    calculate average length of reviews

--- read.py
+++ read.py
@@ -5,5 +5,10 @@ with open('reviews.txt', 'r') as f:
 		data.append(line)
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
-print(data[0])
+print('檔案讀取完了！總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('平均每筆訊息長度為', sum_len / len(data))
+
